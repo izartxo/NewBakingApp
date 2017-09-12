4e34dcdd872a9fe9bbe586f2f3b3f531317843dd
izartxo <izartxo@armiarma.ganbara>
2017-09-12 19:51:48 +0200

    zero

---
+++ NewBakingApp/app/src/test/java/newbaking/code/develop/bizartxo/newbakingapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package newbaking.code.develop.bizartxo.newbakingapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
